830d162c7c3dc1f227de02d945b60cc6d2bf19e1
Lissa Tshona <62402931+lisatshona@users.noreply.github.com>
2024-05-08 11:25:16 +0200

    Update

---
+++ calculator.index/calculator.js
@@ -0,0 +1,19 @@
+//function to display value
+    function dis(val)
+{
+    document.getElementById("result").value+=val
+}
+
+//function that evaluates the digital and return result
+    function solve()
+{
+    let x = document.getElementById("result").value
+    let y = eval(x)
+    document.getElementById("result").value = y
+}
+
+ //function that clear the display
+    function clr()
+{
+    document.getElementById("result").value = ""
+}
